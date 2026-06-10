991f622f8122559b19bb4c37597382be607a6840
Kaustav1409 <kg1409.pvt@gmail.com>
2026-06-11 00:38:22 +0530

    Day 2 Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,11 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import "./App.css";
+const fetchNotes = async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost:5000/api/notes"
+    );
 
-function App() {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  const fetchNotes = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:5000/api/notes"
-      );
-
-      console.log("DATA RECEIVED:", response.data);
-
-      setNotes(response.data);
-    } catch (error) {
-      console.error("Error fetching notes:", error);
-    }
-  };
-
-  return (
-    <div>
-      <h1>SmartNotes</h1>
-
-      <h2>Total Notes: {notes.length}</h2>
-
-      {notes.map((note) => (
-        <div key={note._id}>
-          <h3>{note.title}</h3>
-          <p>{note.description}</p>
-          <hr />
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default App;
+    setNotes(response.data);
+  } catch (error) {
+    console.error("Error fetching notes:", error);
+  }
+};
